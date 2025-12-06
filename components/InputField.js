fc3633dfce069840d7afcf9cfff11a97ce119ae6
aradtah@gmail.com <aradtah@gmail.com>
2025-12-05 22:06:30 -0500

    giving values

--- components/InputField.js
+++ components/InputField.js
@@ -1,2 +1,27 @@
 import React from 'react';
 import { View, Text, TextInput, StyleSheet } from 'react-native';
+
+const InputField = ({
+  label,
+  value,
+  onChangeText,
+  keyboardType = 'default',
+  placeholder,
+  autoCapitalize = 'none',
+  secureTextEntry = false,
+}) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>{label}</Text>
+      <TextInput
+        style={styles.input}
+        value={value}
+        onChangeText={onChangeText}
+        keyboardType={keyboardType}
+        placeholder={placeholder}
+        autoCapitalize={autoCapitalize}
+        secureTextEntry={secureTextEntry}
+      />
+    </View>
+  );
+};
