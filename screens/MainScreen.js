e4028512b7fc1f33337f68db328b1b915f765c25
aradtah@gmail.com <aradtah@gmail.com>
2025-12-05 22:00:15 -0500

    giving the values

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -9,3 +9,19 @@ import {
 import InputField from '../components/InputField';
 
 const API_KEY = 'fca_live_3uSnpdS1Xa8BdmVyM9tRVsv9S7koDW82A0BI2SoZ';
+
+const MainScreen = ({ navigation }) => {
+  const [baseCurrency, setBaseCurrency] = useState('CAD');
+  const [targetCurrency, setTargetCurrency] = useState('');
+  const [amount, setAmount] = useState('1');
+  const [result, setResult] = useState(null);
+  const [rate, setRate] = useState(null);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const isValidCurrencyCode = (code) => /^[A-Z]{3}$/.test(code);
+
+  const isValidAmount = (value) => {
+    const num = Number(value);
+    return !isNaN(num) && num > 0;
+  };
