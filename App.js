25799cb5580f274a15e7320ec956679cd6254497
aradtah@gmail.com <aradtah@gmail.com>
2025-12-05 21:56:28 -0500

    making the library

---
+++ App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import MainScreen from './screens/MainScreen';
+import AboutScreen from './screens/AboutScreen';
