b7d2c5e5d74e62156cf6b95cf3e8b59d605d65a3
aradtah@gmail.com <aradtah@gmail.com>
2025-12-05 21:58:19 -0500

    importing the text, button and style sheet

---
+++ screens/MainScreen.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  ActivityIndicator,
+} from 'react-native';
+import InputField from '../components/InputField';
